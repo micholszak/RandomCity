ff4a8315540ab4ff5b31c1f73425118385d213da
micholszak <michal.olszak@dazn.com>
2020-07-06 23:26:10 +0200

    remove custom navigation bar in favor of toolbar

--- randomcity/src/main/kotlin/com/netguru/randomcity/map/MapFragment.kt
+++ randomcity/src/main/kotlin/com/netguru/randomcity/map/MapFragment.kt
@@ -1,13 +1,19 @@
 package com.netguru.randomcity.map
 
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.netguru.randomcity.R
+import com.netguru.randomcity.util.navigateBack
+import com.netguru.randomcity.util.replace
 
-class MapFragment : SupportMapFragment(), OnMapReadyCallback {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     companion object {
         private const val MAP_KEY = "arguments_map"
@@ -27,20 +33,38 @@ class MapFragment : SupportMapFragment(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mapArguments = arguments?.getParcelable(MAP_KEY)
-        getMapAsync(this)
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_map, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        activity?.actionBar?.apply {
-            mapArguments?.let {
-                title = it.cityName
-                setBackgroundDrawable(ColorDrawable(it.cityColor))
-            }
+        if (savedInstanceState == null) {
+            val supportMapFragment = SupportMapFragment()
+            supportMapFragment.getMapAsync(this)
+            childFragmentManager.replace(R.id.map_container, supportMapFragment)
         }
+        setupHeader(view)
     }
 
     override fun onMapReady(map: GoogleMap) {
         //TODO
+    }
+
+    private fun setupHeader(view: View) {
+        val header: Toolbar? = view.findViewById(R.id.header)
+        if (header != null) {
+            mapArguments?.let {
+                header.title = it.cityName
+                header.setBackgroundColor(it.cityColor)
+            }
+            header.setNavigationOnClickListener {
+                parentFragmentManager.navigateBack()
+            }
+        }
     }
 }
